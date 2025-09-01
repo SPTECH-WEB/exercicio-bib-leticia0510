efb9a47a9447d37b6a5e8dd76e0e390da8b7bacc
leticia0510 <leticia.ssantos@sptech.school>
2025-09-01 20:28:13 -0300

    témino do desafio

--- src/main/java/com/school/sptech/main.java
+++ src/main/java/com/school/sptech/main.java
@@ -1,5 +1,12 @@
 package com.school.sptech;
 
 public class main {
-    //Main
+    public static void main(String[] args) {
+
+        Biblioteca b1 = new Biblioteca("Central", 2.5);
+        b1.registrarLivro(10);
+        b1.emprestar(4);
+        System.out.println(b1.getQtdLivros());
+
+    }
 }
